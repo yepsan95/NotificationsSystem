f14a7563078ebed5c2e5122b77752d35f47bfcf1
Bryan Yep <yepsan95@gmail.com>
2026-08-01 17:40:12 -0500

    feat: create client for real database

---
+++ src/database/real_database.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+from src.database.base_database import BaseDatabaseClient
+
+
+class RealDatabaseClient(BaseDatabaseClient):
+    """Real database client class. This client will be used exclusively for development and production environments."""
+
+
+# Loads variables from the .env file to Python's memory
+load_dotenv(".env")
+
+# Get the database credentials from the environment variables
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+# Construct database URL
+REAL_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Instatiate database client
+real_client = RealDatabaseClient(REAL_DATABASE_URL)
+
+# Store database engine
+db_engine = real_client.engine
+
+# Store session maker configuration
+SessionLocal = real_client.SessionLocal
+
+# Store function to get database client session
+get_db = real_client.get_db
