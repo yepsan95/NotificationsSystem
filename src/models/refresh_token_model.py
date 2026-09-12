de91ea067150a262446d174ac7562d5acb662fd7
Bryan Yep <yepsan95@gmail.com>
2026-09-12 16:02:36 -0500

    feat: create refresh-token entity

---
+++ src/models/refresh_token_model.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from uuid import UUID
+
+from sqlalchemy import Boolean, DateTime, ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.models.base_model import Base
+
+
+class RefreshToken(Base):
+    """Model for entity <RefreshToken>."""
+
+    __tablename__ = "refresh_tokens"
+
+    token: Mapped[str] = mapped_column(String(255), unique=True, index=True, nullable=False)
+    user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    expires_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    is_revoked: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
