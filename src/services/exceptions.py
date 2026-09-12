46133a2ea9023157108023e50f365f5f73fba2bd
Bryan Yep <yepsan95@gmail.com>
2026-09-12 16:07:58 -0500

    feat: create service layer for refresh-token entity

--- src/services/exceptions.py
+++ src/services/exceptions.py
@@ -30,3 +30,21 @@ class DatabaseConnectionError(Exception):
     def __init__(self, message: str):
         self.message = message
         super().__init__(self, "Database connection error. " + self.message)
+
+
+class InvalidRefreshTokenError(Exception):
+    def _init__(self, message: str):
+        self.message = message
+        super().__init__(self, "Refresh token not found. " + self.message)
+
+
+class ExpiredRefreshTokenError(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self, "Refresh token's expiration date has passed. " + self.message)
+
+
+class CompromisedSessionError(Exception):
+    def __init__(self, user_id: UUID):
+        self.user_id = user_id
+        super().__init__(self, f"Compromised session. All tokens must be revoked for user with id '{user_id}'.")
---
+++ src/services/refresh_token_service.py
@@ -0,0 +1,56 @@
+from datetime import datetime, timedelta, timezone
+from uuid import UUID
+
+from fastapi import HTTPException, status
+
+from src.core.security import (
+    REFRESH_TOKEN_EXPIRATION_TIME_IN_DAYS,
+    generate_refresh_token,
+)
+from src.models.refresh_token_model import RefreshToken
+from src.repositories.refresh_token_repository import RefreshTokenRepository
+from src.schemas.auth_schema import RefreshTokenCreate, RefreshTokenUpdate
+from src.services.exceptions import (
+    CompromisedSessionError,
+    ExpiredRefreshTokenError,
+    InvalidRefreshTokenError,
+)
+
+
+class RefreshTokenService:
+    """Service layer for entity <RefreshToken>."""
+
+    def __init__(self, repo: RefreshTokenRepository):
+        self.repo = repo
+
+    def create_token_for_user(self, user_id: UUID) -> str:
+        token = generate_refresh_token()
+        expires_at = (datetime.now(timezone.utc) + timedelta(days=REFRESH_TOKEN_EXPIRATION_TIME_IN_DAYS)).replace(tzinfo=None)
+        new_refresh_token_create_schema = RefreshTokenCreate(
+            token=token,
+            user_id=user_id,
+            expires_at=expires_at,
+            is_revoked=False
+        )
+        self.repo.create(new_refresh_token_create_schema)
+        return token
+
+    def validate_and_rotate_token(self, token: str) -> UUID:
+        db_token = self.repo.get_by_token(token)
+        if not db_token:
+            raise InvalidRefreshTokenError("Token must be revoked.")
+        if db_token.is_revoked:
+            self.repo.revoke_all_user_tokens(db_token.user_id)
+            raise CompromisedSessionError(user_id=db_token.user_id)
+        if db_token.expires_at < datetime.now():
+            raise ExpiredRefreshTokenError("Token must be revoked.")
+        refresh_token_update_schema = RefreshTokenUpdate(is_revoked=True)
+        self.repo.update(db_token, refresh_token_update_schema)
+        return db_token.user_id
+
+    def revoke_token(self, token: str) -> None:
+        db_token = self.repo.get_by_token(token)
+        if not db_token:
+            raise InvalidRefreshTokenError("Cannot be revoked.")
+        refresh_token_update_schema = RefreshTokenUpdate(is_revoked=True)
+        self.repo.update(db_token, refresh_token_update_schema)
